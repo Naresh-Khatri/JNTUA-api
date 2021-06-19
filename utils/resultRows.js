17974118e7caf715c3d3106a8b23dc2118fb605b
Naresh <naresh.khatri2345@gmail.com>
2021-06-19 15:14:59 +0530

    Change /releasedResults endpoint response to match with frontends new select component

--- utils/resultRows.js
+++ utils/resultRows.js
@@ -2,6 +2,8 @@ const JSSoup = require('jssoup').default
 const { parseInt } = require('./utils')
 const axios = require('axios')
 
+let resultsObj = {}
+
 const AllResultsRows = function () {
 
     return new Promise((resolve, reject) => {
@@ -14,13 +16,16 @@ const AllResultsRows = function () {
                 const table = soup.findAll('table')[1]
                 const tr = table.findAll('tr')
                 const resultRows = []
-                for (let i = 1; i < tr.length; i++) {
+                resultsObj={}
+                for (let i = 1; i < 400; i++) {
                     resultRows.push(getResultInfoObj(tr[i].find('a').nextElement._text, tr[i].find('a').attrs.href))
                 }
-                resolve(resultRows)
+                resolve(resultsObj)
             })
             .catch(err => {
                 console.log(err)
+            }).finally(() => {
+                // console.log(JSON.stringify(resultsObj, null, 2));
             })
     })
 
@@ -29,21 +34,41 @@ const AllResultsRows = function () {
         var regulationRegExp = /\((R[^)]+)\)/;
         const splitString = str.toUpperCase().split(' ')
 
-        obj['title'] = str
-        obj['reg'] = !!regulationRegExp.exec(str) ? regulationRegExp.exec(str)[1] : null
-        obj['year'] = splitString[splitString.indexOf('YEAR') - 1] || null
-        obj['sem'] = splitString[splitString.indexOf('SEMESTER') - 1] == 'III' ? "I" :
+        title = obj['title'] = str
+        reg = obj['reg'] = !!regulationRegExp.exec(str) ? regulationRegExp.exec(str)[1] : null
+        year = obj['year'] = splitString[splitString.indexOf('YEAR') - 1] || null
+        sem = obj['sem'] = splitString[splitString.indexOf('SEMESTER') - 1] == 'III' ? "I" :
             splitString[splitString.indexOf('SEMESTER') - 1] == 'IV' ? "II" :
                 splitString[splitString.indexOf('SEMESTER') - 1] || null
-        obj['type'] = splitString.includes('REGULAR') && splitString.includes('SUPPLEMENTARY')
-            ? 'regular & suppy' : splitString.includes('REGULAR') ? 'regular' :
-                splitString.includes('SUPPLEMENTARY') ? 'supply' : null
-        obj['heldOn'] = splitString[splitString.length - 2] + " " + splitString[splitString.length - 1]
-        obj['course'] = splitString[0]
-        obj['resultID'] = parseInt(resultID)
+        course = obj['course'] = splitString[0]
+        resultID = obj['resultID'] = parseInt(resultID)
+        // obj['type'] = splitString.includes('REGULAR') && splitString.includes('SUPPLEMENTARY')
+        //     ? 'regular & suppy' : splitString.includes('REGULAR') ? 'regular' :
+        //         splitString.includes('SUPPLEMENTARY') ? 'supply' : null
+        //obj['heldOn'] = splitString[splitString.length - 2] + " " + splitString[splitString.length - 1]
+
+
+        if (resultsObj[reg]) {
+            if (resultsObj[reg][course]) {
+                if (resultsObj[reg][course][year]) {
+                    if (resultsObj[reg][course][year][sem]) {
+                        resultsObj[reg][course][year][sem].push({ title, resultID })
+                    } else {
+                        resultsObj[reg][course][year][sem] = [{ title, resultID }] 
+                    }
+                } else {
+                    resultsObj[reg][course][year] = { [sem]: [{ title, resultID }] }
+                }
+            } else {
+                resultsObj[reg][course] = { [year]: { [sem]: [{ title, resultID }] } }
+            }
+        } else {
+            resultsObj[reg] = { [course]: { [year]: { [sem]: [{ title, resultID }] } } }
+        }
 
         return obj
     }
 }
+AllResultsRows()
 
 module.exports = { AllResultsRows }
