29abe233114dae3b0d514f6a12151343edcd3e02
Naresh <naresh.khatri2345@gmail.com>
2021-11-21 11:51:59 +0530

    Change order in deploy script

--- deploy-container.sh
+++ deploy-container.sh
@@ -3,8 +3,10 @@
 git pull
 
 container_id=$(sudo docker ps -q --filter ancestor=jntua-api)
+
+sudo docker build -t jntua-api .
+
 sudo docker stop "${container_id}"
 sudo docker rm "${container_id}"
 
-sudo docker build -t jntua-api .
 sudo docker run -it -p 3001:3000 --restart unless-stopped --name=jnuta-api jntua-api
