9d9f8a3938dd8516bf8ba1d14e171979501eaf97
Naresh <naresh.khatri2345@gmail.com>
2021-11-28 23:31:18 +0530

    Change morgan log format to nginx like

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token')
   next()
 })
-app.use(morgan('dev'))
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url" :status :res[content-length] ":referrer" ":user-agent'))
 app.use(express.json());
 app.use('/stats', stats)
 
