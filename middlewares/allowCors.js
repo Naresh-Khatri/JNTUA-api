cec5c7c35544310120ea88e70c799ee5dc229f32
naresh-khatri <naresh.khatri2345@gmail.com>
2022-04-26 23:11:42 +0530

    Add middlewares for logging and cors

---
+++ middlewares/allowCors.js
@@ -0,0 +1,12 @@
+export default function allowCors(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, X-Auth-Token"
+  );
+  next();
+}
