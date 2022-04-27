5bfe1c047ff6d8a390aeb9ac57a307eaa3d270da
naresh-khatri <naresh.khatri2345@gmail.com>
2022-04-28 00:01:14 +0530

    Refactor: dont return promise from getAttempt funtion

--- utils/getAttempts.js
+++ utils/getAttempts.js
@@ -1,68 +1,57 @@
 import axios from "axios";
 import HtmlTableToJson from "html-table-to-json";
 
-export default function getAttempt(result, htn) {
-  return new Promise((resolve, reject) => {
-    try {
-      // console.log('token', token)
-      var config = {
-        method: "get",
-        url: `https://jntuaresults.ac.in/results/res.php?ht=${htn}&id=${result.resultID}&accessToken=${process.env.ACCESS_TOKEN}`,
-        headers: {
-          Cookie: "PHPSESSID=kk98b6kd3oaft9p9p8uiis6ae6;",
-        },
-      };
-      axios(config)
-        .then(async (res) => {
-          // console.log("url",config.url)
-            // console.log("result",result)
-            // console.log("attempt - ",result.resultID, res.data);
+export default async function getAttempt(result, htn) {
+  try {
+    // console.log('token', token)
+    var config = {
+      method: "get",
+      url: `https://jntuaresults.ac.in/results/res.php?ht=${htn}&id=${result.resultID}&accessToken=${process.env.ACCESS_TOKEN}`,
+      headers: {
+        Cookie: "PHPSESSID=kk98b6kd3oaft9p9p8uiis6ae6;",
+      },
+    };
+    const res = await axios(config);
+    // console.log("url",config.url)
+    // console.log("result",result)
+    // console.log("attempt - ",result.resultID, res.data);
 
-          if (res.data == "Something goes wrong 😟") {
-            console.log(res.data);
-            return "Something goes wrong 😟";
-          }
-          //check if token is expired
-          if (res.data == "Invalid Token") {
-            console.log("token expired");
-            return "Token updated please reload";
-          }
-          //reject if result not found
-            // console.log('ressssssssssss:',res.data)
-          if (res.data.includes("Invalid")) {
-              // console.log(res.data)
-            // console.log(`Invalid htn=${htn} with resid=${result.resultID}`)
-            return resolve({});
-          }
-
-          //get stud name in all attempts
-          let studInfo = res.data
-            .substring(0, res.data.indexOf("<br>"))
-            .replace(
-              /&nbsp;|<\/?[^>]+(>|$)|Hall Ticket No :|Student name:/g,
-              ""
-            )
-            .split(" ");
-          studInfo.shift();
-          const studName = studInfo.join(" ");
-
-          let tableHTML = res.data + "</th></tr></table>";
-
-          //jntua is a fucking piece of shit for not adding these closing tags
-          //such a pain
-          const resultObj = getAllAttemptsObj(tableHTML, result);
-        //   console.log(resultObj)
-          return resolve({ resultObj, studName });
-        })
-        .catch((err) => {
-          console.log(err);
-          reject(err);
-          return;
-        });
-    } catch (err) {
-      console.log(err);
+    if (res.data == "Something goes wrong 😟") {
+      console.log(res.data);
+      return "Something goes wrong 😟";
     }
-  });
+    //check if token is expired
+    if (res.data == "Invalid Token") {
+      console.log("token expired");
+      return "Token updated please reload";
+    }
+    //reject if result not found
+    // console.log('ressssssssssss:',res.data)
+    if (res.data.includes("Invalid")) {
+      // console.log(res.data)
+      // console.log(`Invalid htn=${htn} with resid=${result.resultID}`)
+      return {};
+    }
+
+    //get stud name in all attempts
+    let studInfo = res.data
+      .substring(0, res.data.indexOf("<br>"))
+      .replace(/&nbsp;|<\/?[^>]+(>|$)|Hall Ticket No :|Student name:/g, "")
+      .split(" ");
+    studInfo.shift();
+    const studName = studInfo.join(" ");
+
+    let tableHTML = res.data + "</th></tr></table>";
+
+    //jntua is a fucking piece of shit for not adding these closing tags
+    //such a pain
+    const resultObj = getAllAttemptsObj(tableHTML, result);
+    //   console.log(resultObj)
+    return { resultObj, studName };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 }
 function getAllAttemptsObj(tableHTML, result) {
   //when Grade is AB jntu calls it 'Grades' 😥
